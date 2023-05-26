4fa14ec46ec7f3d878f04e13e788f4505eb4e6fd
safatsaat2 <sakibsafat47@gmail.com>
2023-05-26 15:44:34 +0600

    private to public

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const cors = require('cors')
+const chefs = require('./chef.json')
+const recipes = require('./recipe.json')
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/chefs', (req, res)=>{
+    res.send(chefs)
+});
+app.get('/recipes', (req, res)=>{
+  res.send(recipes)
+});
+app.get('/chefs/:id', (req, res)=>{
+  const id = req.params.id;
+  const chef = chefs.find(chef => chef._id === id)
+  res.send(chef)
+}
+)
+app.get('/recipe/:id',(req, res)=>{
+  const id = req.params.id;
+  const chefRecipe = recipes.filter(recipe=> recipe.chefId === id)
+  res.send(chefRecipe)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
